55631bdc8b49c0c1d53b3da0eea7fba0bd0ec46c
Sergio994350 <tarasov-mnm@yandex.ru>
2022-09-19 20:37:17 +0300

    initial commit

---
+++ app/src/main/java/com/epicteam1/mountainskiplaces/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.epicteam1.mountainskiplaces.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
